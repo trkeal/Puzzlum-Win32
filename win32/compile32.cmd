86012ac1ed22c2f0ef8141e476eae764730c9a07
trkeal <95458960+trkeal@users.noreply.github.com>
2021-12-16 10:23:21 -0700

    Updated debug
    Updated debug  "2>&1"

--- win32/compile32.cmd
+++ win32/compile32.cmd
@@ -10,35 +10,35 @@ echo ===[ Puzzlum-Win32 ]==[ FreeBASIC x86 Linker Utility ]=== >> %log%
 
 echo: >> %log%
 echo ===[ Constants ]==[ %static% ]=== >> %log%
-%fbc% "const.bas" -lib -v -RR >> %log%
+%fbc% "const.bas" -lib -v -RR >> %log% 2>&1
 
 echo: >> %log%
 echo ===[ Names Table ]==[ %static% ]=== >> %log%
-%fbc% "names.bas" -lib -v -RR >> %log%
+%fbc% "names.bas" -lib -v -RR >> %log% 2>&1
 
 echo: >> %log%
 echo ===[ Composite Layer Video ]==[ %static% ]=== >> %log%
-%fbc% "clv.bas" -lib -v -RR >> %log%
+%fbc% "clv.bas" -lib -v -RR >> %log% 2>&1
 
 echo: >> %log%
 echo ===[ FBImage ]==[ %static% ]=== >> %log%
-%fbc% "fbimage.bas" -lib -v -RR >> %log%
+%fbc% "fbimage.bas" -lib -v -RR >> %log% 2>&1
 
 echo: >> %log%
 echo ===[ Puzzlum / Vars ]==[ %static% ]=== >> %log%
-%fbc% "puzzlum-vars.bas" -lib -v -RR >> %log%
+%fbc% "puzzlum-vars.bas" -lib -v -RR >> %log% 2>&1
 
 echo: >> %log%
 echo ===[ Puzzlum / Subs ]==[ %static% ]=== >> %log%
-%fbc% "puzzlum-subs.bas" -lib -v -RR >> %log%
+%fbc% "puzzlum-subs.bas" -lib -v -RR >> %log% 2>&1
 
 echo: >> %log%
 echo ===[ Puzzlum / Task Manager ]==[ %static% ]=== >> %log%
-%fbc% "puzzlum-task.bas" -lib -v -RR >> %log%
+%fbc% "puzzlum-task.bas" -lib -v -RR >> %log% 2>&1
 
 echo: >> %log%
 echo ===[ Puzzlum / Main ]==[ %binary% ]=== >> %log%
-%fbc% "puzzlum-win32.bas" -v -RR -s gui ".\win32\rc\puzzlum.rc" >> %log%
+%fbc% "puzzlum-win32.bas" -v -RR -s gui ".\win32\rc\puzzlum.rc" >> %log% 2>&1
 
 echo: >> %log%
 echo ===[ Now attempting to load the "puzzlum-win32" executable.. ]=== >> %log%
